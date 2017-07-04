9628b0c642cf37cc043a8321fd505ff391268bc8
Giuseppe Scrivano <gscrivan@redhat.com>
2017-07-04 13:23:51 +0200

    demos: add demo for running systemd based images
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>
    
    Closes: #4
    Approved by: yuqi-zhang

---
+++ demos/run-systemd/run_demo.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+rm -rf cache
+mkdir cache
+bwrap --ro-bind / / --dev /dev --proc /proc --bind $(pwd) $(pwd) skopeo copy docker://fedora oci:cache
+
+mkdir rootfs
+
+(
+    cd rootfs
+    # Hack, this works only because fedora is a single layer image
+    for i in ../cache/blobs/sha256/*
+    do
+        bwrap --ro-bind / / --dev /dev --proc /proc --bind $(pwd) $(pwd) tar xf $i || true
+    done
+    mkdir {dev,proc,sys}
+)
+
+bwrap --uid 0 --gid 0 --unshare-user --bind rootfs / --dev /dev --proc /proc --tmpfs /run --tmpfs /var --tmpfs /var/log systemd-tmpfiles --create
+
+bwrap --uid 0 --gid 0 --unshare-user --bind rootfs / --dev /dev --proc /proc --tmpfs /run --tmpfs /var --tmpfs /var/log systemctl mask dev-hugepages.mount
+
+bwrap-oci --pid-file=/tmp/pidfile
+
+kill -37 $(cat /tmp/pidfile)
